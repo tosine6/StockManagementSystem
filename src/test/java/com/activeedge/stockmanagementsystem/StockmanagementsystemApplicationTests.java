0e26e115352f176237e01d89496fe6ff4541a94a
tosine6 <adetosine6@gmail.com>
2021-09-25 13:28:02 +0100

    first commit

---
+++ src/test/java/com/activeedge/stockmanagementsystem/StockmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activeedge.stockmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
